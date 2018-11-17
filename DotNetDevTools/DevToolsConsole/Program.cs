cea342df56bb25f1e5a81f50496e9480a94e5da1
Kahuunao <clown_90@hotmail.fr>
2018-11-17 14:21:30 +0100

    Commit initial

---
+++ DotNetDevTools/DevToolsConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevToolsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
